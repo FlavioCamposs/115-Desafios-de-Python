013733a9c1c8459467c6b2779a55d236631af19c
Flávio Batista Campos Ferreira <155656601+FlavioCamposs@users.noreply.github.com>
2024-01-05 12:09:21 -0300

    06 – Dobro, Triplo, Raiz Quadrada.py

---
+++ 06 – Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1**(1/2)
+print('Seu dobro é: {}, seu triplo é: {} e sua raiz quadrada é: {:.2f}'.format(dobro, triplo, raiz))
--- desafio 06.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite um numero: '))
-dobro = n1 * 2
-triplo = n1 * 3
-raiz = n1**(1/2)
-print('Seu dobro é: {}, seu triplo é: {} e sua raiz quadrada é: {:.2f}'.format(dobro, triplo, raiz))
